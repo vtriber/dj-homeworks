81369a1605f74e95b48bbf9c1efb089dff225903
vtriber <vtriber@ya.ru>
2023-01-17 15:48:47 +0300

    orm_migrations completed new

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'SuperPython43',
+        'USER': 'vtriber',
+        'PASSWORD': '1',
     }
 }
 
