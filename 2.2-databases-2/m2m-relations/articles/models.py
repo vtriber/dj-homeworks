73906bc0b8047db2eaed329f0a3407be98359bb2
vtriber <vtriber@ya.ru>
2023-01-29 23:38:17 +0300

    m2m - admin complit 2.0

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -4,15 +4,18 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=20, verbose_name='Тег',)
 
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
 
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
-
 
     class Meta:
         verbose_name = 'Статья'
@@ -23,10 +26,11 @@ class Article(models.Model):
 
 
 
-
-
-
 class Scope(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE,related_name='scopes', verbose_name='Раздел',)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел',)
     is_main = models.BooleanField(verbose_name='Основной',)
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
